e5d4e2e8034d11a75bf39ce316749b28f439d940
chriski28 <75042441+chriski28@users.noreply.github.com>
2020-11-26 20:05:48 +0100

    Update ChestFillerData.java
    Hab jetzt grob einmal die Tiers mit meinen Ideen gefüllt. 
    Die Enchantments würd ich auf jeden Fall noch überarbeiten und Potions/Splash Potions hinzufügen.
    Auch die Gold Balance ist natürlich noch irgendwie, aber sowohl die Damage als auch die Protection steigt relativ gleichmäßig (aber vielleicht wollen wir krassere Sprünge zwischen den Tiers)
    Außerdem war meine Idee in die Defender buckets einfach nur Gold reinzugeben. Denn die Verteidiger haben ja eh essen etc in der Basis. Ich würde ihnen auch unendlich Rüstungen, Pfeile etc zur Verfügung stellen oder sie im Laden kaufen lassen. Nur sind alle Defender items automatisch mit dem Curse of Vanishing ausgestattet. 
    Das würde die Balancing issues beheben.

--- src/main/java/dev/laurenz/mcpaperretake/command/ChestFillerData.java
+++ src/main/java/dev/laurenz/mcpaperretake/command/ChestFillerData.java
@@ -23,11 +23,20 @@ public class ChestFillerData {
     public ChestFillerData() {
 
         ATK_TIER1_ITEMS = new ItemStack[] {
-                ItemStackFactory.create(Material.LEATHER_BOOTS).get()
+                ItemStackFactory.create(Material.LEATHER_BOOTS).get(),
+                ItemStackFactory.create(Material.LEATHER_CHESTPLATE).get(),
+                ItemStackFactory.create(Material.LEATHER_HELMET).get(),
+                ItemStackFactory.create(Material.LEATHER_LEGGINGS).get(),
+                ItemStackFactory.create(Material.BREAD, 5).get(),
+                ItemStackFactory.create(Material.STONE_SWORD).get(),
+                ItemStackFactory.create(Material.ARROW, 4).get(),
+                ItemStackFactory.create(Material.TORCH, 2).get()
+                
         };
 
         DEF_TIER1_ITEMS = new ItemStack[] {
-                ItemStackFactory.create(Material.IRON_SWORD).addEnchantment(Enchantment.KNOCKBACK, 1).get()
+                ItemStackFactory.create(Material.GOLD_INGOT, 3).get()
+                
         };
 
         RANDOM_TIER1_ITEMS = Utils.concatItemStacks(ATK_TIER1_ITEMS, DEF_TIER1_ITEMS);
@@ -35,11 +44,20 @@ public class ChestFillerData {
         // --
 
         ATK_TIER2_ITEMS = new ItemStack[] {
-                ItemStackFactory.create(Material.CHAINMAIL_HELMET).get()
+                ItemStackFactory.create(Material.STONE_SWORD).addEnchantment(Enchantment.KNOCKBACK, 1).get(),
+                ItemStackFactory.create(Material.LEATHER_BOOTS).addEnchantment(Enchantment.PROTECTION_PROJECTILE, 2).get(),
+                ItemStackFactory.create(Material.LEATHER_CHESTPLATE).addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2).get(),
+                ItemStackFactory.create(Material.LEATHER_HELMET).addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2).get(),
+                ItemStackFactory.create(Material.LEATHER_LEGGINGS).addEnchantment(Enchantment.PROTECTION_PROJECTILE, 2).get(),
+                ItemStackFactory.create(Material.STONE_AXE).get(),
+                ItemStackFactory.create(Material.BOW).get(),
+                ItemStackFactory.create(Material.COOKED_MUTTON, 4).get(),
+                ItemStackFactory.create(Material.SPECTRAL_ARROW, 4).get(),
+                
         };
 
         DEF_TIER2_ITEMS = new ItemStack[] {
-                ItemStackFactory.create(Material.IRON_SWORD).addEnchantment(Enchantment.VANISHING_CURSE, 1).get()
+                ItemStackFactory.create(Material.GOLD_INGOT, 9).get()
         };
 
         RANDOM_TIER2_ITEMS = Utils.concatItemStacks(ATK_TIER2_ITEMS, DEF_TIER2_ITEMS);
@@ -49,12 +67,24 @@ public class ChestFillerData {
         // --
 
         ATK_TIER3_ITEMS = new ItemStack[] {
-                ItemStackFactory.create(Material.CHAINMAIL_CHESTPLATE).addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2).get(),
-                ItemStackFactory.create(Material.CHAINMAIL_CHESTPLATE).addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1).get()
+                ItemStackFactory.create(Material.CHAINMAIL_CHESTPLATE).get(),
+                ItemStackFactory.create(Material.CHAINMAIL_HELMET).get(),
+                ItemStackFactory.create(Material.CHAINMAIL_LEGGINGS).get(),
+                ItemStackFactory.create(Material.CHAINMAIL_BOOTS).get(),
+                ItemStackFactory.create(Material.CROSSBOW).get(),
+                ItemStackFactory.create(Material.COOKED_BEEF, 6).get(),
+                ItemStackFactory.create(Material.BOW).addEnchantment(Enchantment.ARROW_FIRE, 1).get(),
+                ItemStackFactory.create(Material.STONE_AXE).addEnchantment(Enchantment.KNOCKBACK, 1).get(),
+                ItemStackFactory.create(Material.IRON_SWORD).get(),
+                ItemStackFactory.create(Material.SHIELD).get(),
+                ItemStackFactory.create(Material.LADDER, 3).get(),
+                ItemStackFactory.create(Material.GOLDEN_APPLE).get(),
+                ItemStackFactory.create(Material.BIRCH_BUTTON).get(),
+
         };
 
         DEF_TIER3_ITEMS = new ItemStack[] {
-                ItemStackFactory.create(Material.IRON_SWORD).addEnchantment(Enchantment.KNOCKBACK, 1).get()
+                ItemStackFactory.create(Material.GOLD_INGOT, 27).get()
         };
 
         RANDOM_TIER3_ITEMS = Utils.concatItemStacks(ATK_TIER3_ITEMS, DEF_TIER3_ITEMS);
